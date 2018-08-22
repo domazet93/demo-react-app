c7a791d1236fc31517233f4e73df20f3ff5dd8b3
Toni Domazet <toni.domaz93@hotmail.com>
2018-08-22 21:41:50 +0100

    handling events with handler methods adding external style file and inline style inside comp.

--- src/App.js
+++ src/App.js
@@ -10,21 +10,53 @@ class App extends Component {
       { name: "Marijana", age: 20 }
     ]
   }
-
    
-  switchNameHandler = () => {
-    console.log("Was clicked")
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons:  [
+        { name: newName, age: 24 },
+        { name: "Ivan", age: 27 }, 
+        { name: "Marijana", age: 20 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons:  [
+        { name: "Toni", age: 24 },
+        { name: event.target.value, age: 27 }, 
+        { name: "Marijana", age: 20 }
+      ]
+    })
   }
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "4px",
+      cursor: "pointer"
+    }
+
     return (
       <div className="App">
         <h1>Welcome to React</h1>
         <p>I am React App</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobby: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler("Antony")}
+                style={style}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Ton")}
+          changed={this.nameChangedHandler}>My Hobby: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
