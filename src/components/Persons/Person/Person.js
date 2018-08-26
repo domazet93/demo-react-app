f1beacc2ef8e84e5cd997eb2e7caba0c7006005f
Toni Domazet <toni.domaz93@hotmail.com>
2018-08-26 18:34:05 +0100

    props-type package for checking type of prop values using ref attr to focus input el. after comp. mount

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types"
 import clasess from "./Person.css"
 import Aux from "../../../hoc/Aux"
 import withClass from "../../../hoc/withClass"
@@ -15,6 +16,7 @@ class Person extends Component {
 
   componentDidMount = () => {
     console.log("[Person.js] componentDidMount()")
+    this.inputElement.focus();
   }
   render() {
     console.log("[Person.js] render()")
@@ -23,10 +25,20 @@ class Person extends Component {
       <Aux>
         <p onClick={this.props.click}>I am {this.props.name} and I am  {this.props.age} years old.</p>
         <p>{this.props.children }</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+        <input type="text" 
+              ref={(inp) => { this.inputElement = inp }}
+              onChange={this.props.changed} 
+              value={this.props.name}></input>
       </Aux>
     )
   }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 }
 
 export default withClass(Person, clasess.Person);
